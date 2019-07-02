afd6ded1aed25d00b8136bac2daa3aef4bc307ba
lilia148 <51103968+lilia148@users.noreply.github.com>
2019-07-02 08:15:42 -0500

    Add files via upload

---
+++ ejercicio_nuevo.py
@@ -0,0 +1,53 @@
+comidas = ["papas fritas", "pollo frito", "camarones,"]
+
+def imprimir_menu(): 
+	print("1 listar menu ")
+	print("2 agregar pedido")
+	print("3 imprimir pedido")
+	print("4 salir")
+	valor = input("ingrese el numero de la accion: ")
+ 	return =valor 
+
+
+def listar_menu():
+	print() 
+	print("listado de menu") 
+	for productos in range (len(menus)):
+		print("{0} {1}" .format(producto,
+menus[ppreoducto])
+
+def agregar_pedido():
+	print()
+	print("creando un pedido")
+	mesa = input ("numero de mesa")
+	producto = input("nombre de platillo")
+	pedidos.append("mesa No. {0} platillo {1} .format(mesa,producto))
+
+def imprimer_pedido():
+	if pedidios:
+		for pedido in pedidos:
+			print(pedido)
+	else:
+		print("no hay pedidos ingresados")
+
+continuar = True 
+
+while continuar: 
+	valor_retornado = valor retornado 
+	v_retornado = imprimir_menu()
+	if int(v_retornado) == 1 :
+		for menus in menus 
+			print(menu)
+	elif int(v_retornado) == 2 :
+		menu = []
+	elif int(v_retornado) == 3 :
+		valor = input("ingrese el menu a agregar :")
+
+		producto . append (valor)
+	elif int(v_retornado) == 4 :
+		continuar = False
+
+	else:
+		print("opcion no controlada")
+
+ 
